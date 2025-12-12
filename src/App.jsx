928d8c71dbc0245cb6b92481c07c6ab6f7f567e8
ranjeetchauhan <ranjeetchauhan123>
2025-12-12 08:20:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MovieSearch from './component/MovieSearch'
+
+
+function App() {
+
+  return (
+    <div>
+      <MovieSearch />
+    </div>
+  )
+}
+
+
+export default App
